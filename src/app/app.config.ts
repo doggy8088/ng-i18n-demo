2380bf171fe9bedddf0c35c9a9482dfdd6619661
Will <doggy.huang@gmail.com>
2024-01-27 23:14:58 +0800

    在專案中加入指定的 Locale 設定

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { DATE_PIPE_DEFAULT_OPTIONS, registerLocaleData } from '@angular/common';
+
+import localeZhHant from '@angular/common/locales/zh-Hant';
+import localZhHantExtra from '@angular/common/locales/extra/zh-Hant';
+console.log(localeZhHant, localZhHantExtra);
+registerLocaleData(localeZhHant, localZhHantExtra);
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'zh-Hant',
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: {
+        dateFormat: 'full',
+        timezone: '+0800',
+      },
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'TWD',
+    },
+  ],
 };
